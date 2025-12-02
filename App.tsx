543a50ac6bb80a2d77c1319ee1e6e75c30f80eeb
NguyenVu2005 <nvu22@lsu.edu>
2025-12-01 23:03:36 -0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import RootNavigator from './src/navigation/RootNavigator';
+import { supabase } from './src/lib/supabase';
+import { navigationRef } from './src/navigation/navigationRef';
 import React from "react";
 import DiscoverScreen from "./src/screens/DiscoverScreen";
 
